c31f31006e4217d9412949697f5bd152fd1ea76c
QGQYY <2970663121@qq.com>
2019-08-01 00:01:19 +0800

    Updates

---
+++ precache-manifest.e12102f6ac77ece41e9aa8f50a895784.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "34b656fedf1347938002d5c8ab118201",
+    "url": "/FirstAPP/static/media/YuShouJinShu.34b656fe.ttf"
+  },
+  {
+    "revision": "0e935cb7ccfe55f906ef",
+    "url": "/FirstAPP/static/css/main.2255516c.chunk.css"
+  },
+  {
+    "revision": "b3ab26228da1ea8ca71d",
+    "url": "/FirstAPP/static/js/1.b3ab2622.chunk.js"
+  },
+  {
+    "revision": "87917601aad752876428",
+    "url": "/FirstAPP/static/js/runtime~main.87917601.js"
+  },
+  {
+    "revision": "7cc3825b46fd8e497faa3ea9349a0004",
+    "url": "/FirstAPP/static/media/JiangNanShouShu.7cc3825b.ttf"
+  },
+  {
+    "revision": "ffd5cb30df3549feb0d04816d7697653",
+    "url": "/FirstAPP/static/media/YunXiJinShu.ffd5cb30.ttf"
+  },
+  {
+    "revision": "0e935cb7ccfe55f906ef",
+    "url": "/FirstAPP/static/js/main.0e935cb7.chunk.js"
+  },
+  {
+    "revision": "299db1c83f330895e6889721dd00d488",
+    "url": "/FirstAPP/static/media/JingHongShouShu.299db1c8.ttf"
+  },
+  {
+    "revision": "42c04bc75b5848bf7411786f954d0271",
+    "url": "/FirstAPP/static/media/WuLingJiangHu.42c04bc7.ttf"
+  },
+  {
+    "revision": "f3e7a06579116916d528624f574edd04",
+    "url": "/FirstAPP/static/media/XiaoYaoXingShu.f3e7a065.ttf"
+  },
+  {
+    "revision": "2ff7d2f8718de7290766bd1729cb7ca2",
+    "url": "/FirstAPP/static/media/ZhengKai.2ff7d2f8.ttf"
+  },
+  {
+    "revision": "d63578656b686f461e7d8471ee1fc6d9",
+    "url": "/FirstAPP/static/media/background.d6357865.jpg"
+  },
+  {
+    "revision": "c2e30a4d7746960e63c65d42fcadaa8d",
+    "url": "/FirstAPP/index.html"
+  }
+];
